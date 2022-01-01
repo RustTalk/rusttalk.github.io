7a4bfef55fa4e26ea22c7ec6e297b871a0082b78
jiacai2050 <dev@liujiacai.net>
2022-01-01 22:24:52 +0800

    release podcast 000

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((eval . (add-hook 'before-save-hook 'time-stamp nil t)))))
---
+++ Makefile
@@ -0,0 +1,5 @@
+serve:
+	hugo serve -D
+
+init:
+	git submodule update --init
